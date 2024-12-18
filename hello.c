fb64c2ca2afe9b01b2edd9c5d3cf0cf8a5fa1457
PANNE008 <pannellajacopo@gmail.com>
2024-12-18 13:14:19 +0000

    napoli

--- hello.c
+++ hello.c
@@ -1,64 +1,35 @@
+
 #include <stdio.h>
 
-// Funzione per simulare la porta NOT
-int NOT(int a) {
-    // Il valore di ritorno della funzione
-    return 1 - a;
-}
-
-// Funzione per simulare la porta AND
-int AND(int a, int B) {
-    // Il valore di ritorno della funzione
-    // TODO aggiungere l'espressione matematica per soddisfare la porta AND
-    return 1337;
-}
-
-// Funzione per simulare la porta OR
-int OR(int a, int B) {
-    // Il valore di ritorno della funzione
-    // TODO aggiungere l'espressione matematica per soddisfare la porta OR
-    return 1337;
-}
-
 int main() {
-    // Dichiara una variabile
-    int A, B;
+    int input[6];  
+    int result = 1;  
 
-    // Mostra un messaggio sul terminale che l'utente può leggere
-    printf("Inserisci il valore di A (0 o 1):\n");
-    // Attende che l'utente inserisca un valore e prema invio
-    scanf("%d", &A);
-    // Mostra un messaggio sul terminale che l'utente può leggere
-    printf("Hai inserito il valore A: %d\n", A);
+    
+    printf("Inserisci 6 valori che siano 0 o 1: ");
+    for (int i = 0; i < 6; i++) {
+        scanf("%d", &input[i]);
 
-    // Mostra un messaggio sul terminale che l'utente può leggere
-    printf("Inserisci il valore di B (0 o 1):\n");
-    // Attende che l'utente inserisca un valore e prema invio
-    scanf("%d", &B);
-    // Mostra un messaggio sul terminale che l'utente può leggere
-    printf("Hai inserito il valore B: %d\n", B);
+        
+        if (input[i] != 0 && input[i] != 1) {
+            printf("Valore non valido, inserisci 0 o 1.\n");
+            return 1;  
+        }
 
-    if (A != 0 && A != 1 && B != 0 && B != 1) {
-        printf("I valori inseriti non sono 1 o 0\n");
-    } else {
-        // Procediamo con l'esecuzione solo se il numero è 1 o 0
-        printf("I valori inseriti sono 1 o 0\n");
-
-        // Esempio di porta NOT
-        printf("Il valore di A viene trasformato da una porta NOT\n");
-        int notA = NOT(A);
-        printf("Il valore di uscita della porta NOT è: %d\n", notA);
-
-        // Esempio di porta AND
-        printf("I valori di A e B vengono trasformati da una porta AND\n");
-        int andAB = AND(A, B);
-        printf("Il valore di uscita della porta AND è: %d\n", andAB);
-
-        // Esempio di porta OR
-        printf("I valori di A e B vengono trasformati da una porta OR\n");
-        int orAB = OR(A, B);
-        printf("Il valore di uscita della porta OR è: %d\n", orAB);
+      
+        if (input[i] == 0) {
+            result = 0;
+        }
     }
 
-    printf("\n\n");
-}
+    
+    printf("Il risultato del labirinto è: %d\n", result);
+
+    if (result == 1) {
+        printf("Labirinto risolto!\n");
+    } else {
+        printf("Labirinto non risolto.\n");
+    }
+
+    return 0;
+} 
